9d1a001220a3309d1a568fc22c8bc30092db04d3
avinash1598 <91avinash76@gmail.com>
2026-03-11 09:52:44 -0500

    Subject 2 Jonathan session 6

--- ExpScript/CORNFB.m
+++ ExpScript/CORNFB.m
@@ -127,7 +127,7 @@ stimSpread = [5, 25]; % 5, 25, 45                                          % Sti
 stimContrast = [0.015, 0.05];                                               % 0.022 - seems good contrast level to get 50:50 conf report (HC:Lc)
 respMaxDur = 5;                                                            % 0.010 Maximum allowed time for user to respond (2 seconds)
 respSuccessWaitDur = 0.5;
-numBlocks   = 6; %6;                                                       % Number of blocks 
+numBlocks   = 7; %6;                                                       % Number of blocks 
 % nTrialsPerBlock = numel(stimOrientations)*numel(stimSpread)*numel(stimContrast)*numel(stimDur);    % Assuming each trial takes max of 5 second, a block should take ~8 minutes
 % nTrialsPerBlock = numel(stimOrientations)*8;                             % TODO: delete
 % nTrialsPerBlock = numel(stimOrientations)*2; % Train
@@ -207,8 +207,8 @@ trlStatusVec  = trlStatus*ones(nTrials, 1);
 
 % 0.3000    30.0000     0.0600
 
-% Reduce contrast (0.018) and increase duration (0.1200) - for the next
-% subject
+
+% Jonathan
 combinations = [
     0.3000    10.0000     0.0600
     0.3000    30.0000     0.0600
@@ -219,6 +219,22 @@ combinations = [
     0.0800    30.0000     0.0220
 ];
 
+% % Reduce contrast (0.018) and increase duration (0.1200) - for the next
+% % subject
+% % David
+% % Stim value changed
+% combinations = [
+%     0.3000    10.0000     0.0400
+%     0.3000    30.0000     0.0400
+%     0.3000    10.0000     0.0180
+%     0.3000    30.0000     0.0180
+%     
+%     0.1000    10.0000     0.0180
+%     0.1000    30.0000     0.0180
+% ];
+
+
+    
 %  % 0.0800    20.0000    0.0220
 %     % 0.0800    30.0000    0.0250
 %     %0.3000    20.0000    0.0500  %%%%% Easiest
